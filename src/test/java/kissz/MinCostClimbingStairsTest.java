6714726864dfa69c29a4155deb0558614df16557
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2019-07-15 19:15:40 +0100

    MinCostClimbingStairs

---
+++ src/test/java/kissz/MinCostClimbingStairsTest.java
@@ -0,0 +1,22 @@
+package kissz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MinCostClimbingStairsTest {
+    private MinCostClimbingStairs mc = new MinCostClimbingStairs();
+
+    @Test
+    public void example1() {
+        var r = mc.minCostClimbingStairs(new int[]{10, 15, 20});
+        assertEquals(15, r);
+    }
+
+    @Test
+    public void example2() {
+        var r = mc.minCostClimbingStairs(new int[]{1, 100, 1, 1, 1, 100, 1, 1, 100, 1});
+        assertEquals(6, r);
+    }
+
+}
