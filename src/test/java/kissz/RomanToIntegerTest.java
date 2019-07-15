440ff2217e8641c74d16478db275a6a59020f629
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2019-07-15 22:13:04 +0100

    RomanNumber

--- src/test/java/kissz/RomanToIntegerTest.java
+++ src/test/java/kissz/RomanToIntegerTest.java
@@ -2,11 +2,49 @@ package kissz;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.*;
 
 public class RomanToIntegerTest {
     @Test
     public void testRomanToValue() {
         assertEquals(1998, new RomanToInteger().romanToInt("MCMXCVIII"));
+    }
+
+    private RomanToInteger r = new RomanToInteger();
+
+    @Test
+    public void parsesZero() {
+        RomanNumber number = new RomanNumber(RomanNumber.ZERO_VALUE);
+
+        assertEquals(0, r.romanToInt(""));
+    }
+
+    @Test
+    public void parsesSymbols() {
+        Arrays.stream(RomanSymbol.values())
+                .forEach(symbol -> {
+                    assertEquals(symbol.getValue(), r.romanToInt(symbol.name()));
+                });
+    }
+
+    @Test
+    public void parsesTwo() {
+        assertEquals(2, r.romanToInt("II"));
+    }
+
+    @Test
+    public void parsesNine() {
+        assertEquals(9, r.romanToInt("IX"));
+    }
+
+    @Test
+    public void shouldParseGivenExamples() {
+        assertEquals(1999, r.romanToInt("MCMXCIX"));
+        assertEquals(2016, r.romanToInt("MMXVI"));
+        assertEquals(944, r.romanToInt("CMXLIV"));
+        assertEquals(470, r.romanToInt("CDLXX"));
+        assertEquals(88, r.romanToInt("LXXXVIII"));
     }
 }
