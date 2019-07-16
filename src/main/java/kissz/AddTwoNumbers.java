d8fd24fc2f57851ddaeb97cdc8d47575c50020e8
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2019-07-16 22:23:29 +0100

    AddTwoNumbers

---
+++ src/main/java/kissz/AddTwoNumbers.java
@@ -0,0 +1,63 @@
+package kissz;
+
+class ListNode {
+    static ListNode fromInt(int n) {
+        if (n == 0) return new ListNode(0);
+
+        final ListNode lastDigit = new ListNode(n % 10);
+        ListNode c = lastDigit;
+        do {
+            n /= 10;
+            c.next = new ListNode(n % 10);
+            c = c.next;
+        } while (n > 0);
+        return lastDigit;
+    }
+
+    int val;
+    ListNode next;
+
+    ListNode(int x) { val = x; }
+
+    public int toInt() {
+        int m = 1;
+        ListNode c = this;
+        int ret = 0;
+        while (c != null) {
+            ret += c.val * m;
+            m *= 10;
+            c = c.next;
+        }
+        return ret;
+    }
+}
+
+public class AddTwoNumbers {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode lastDigit = null;
+        ListNode curr = null;
+        int c = 0;
+        do {
+            int a = l1 != null ? l1.val : 0;
+            int b = l2 != null ? l2.val : 0;
+
+            int sum = a + b + c;
+            if (curr != null) {
+                curr.next = new ListNode(0);
+                curr = curr.next;
+            }
+            if (lastDigit == null) {
+                lastDigit = new ListNode(0);
+                curr = lastDigit;
+            }
+            curr.val = sum % 10;
+            if (sum >= 10) c = 1;
+            else c = 0;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        } while (l1 != null || l2 != null || c != 0);
+
+        return lastDigit;
+    }
+}
---
+++ src/test/java/kissz/AddTwoNumbersTest.java
@@ -0,0 +1,16 @@
+package kissz;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AddTwoNumbersTest {
+    @Test
+    public void example() {
+        var r = new AddTwoNumbers().addTwoNumbers(
+                ListNode.fromInt(342),
+                ListNode.fromInt(465)
+        );
+        assertEquals(807, r.toInt());
+    }
+}
