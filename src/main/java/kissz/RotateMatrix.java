f65ff22913a6ef0e0cb1fa03a0f9267507e267d7
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2019-07-22 20:13:41 +0100

    remove separate middle row handling

--- src/main/java/kissz/RotateMatrix.java
+++ src/main/java/kissz/RotateMatrix.java
@@ -2,12 +2,13 @@ package kissz;
 
 public class RotateMatrix {
     public static void rotate90(int[][] mx) {
-        final int sq = mx.length / 2;
-        final int a = sq * sq;
+        final int cols = mx.length / 2 + mx.length % 2; // handles middle row in case odd
+        final int rows = mx.length / 2;
+        final int a = cols * rows;
 
         for (int id = 0; id < a; id++) {
-            int col = id % sq;
-            int row = id / sq;
+            int col = id % cols;
+            int row = id / cols;
 
             // top left
             int tmp = mx[row][col];
@@ -18,23 +19,6 @@ public class RotateMatrix {
             // bottom left
             mx[mx.length - 1 - row][mx.length - 1 - col] = mx[mx.length - 1 - col][row];
             mx[mx.length - 1 - col][row] = tmp;
-        }
-
-        if (mx.length % 2 == 1) {
-            // do middle columns
-            final int mid = sq;
-            for (int i = 0; i < sq; i++) {
-                // top
-                int tmp = mx[i][mid];
-                // top <- right
-                mx[i][mid] = mx[mid][mx.length - 1 - i];
-                // right <- bottom
-                mx[mid][mx.length - 1 - i] = mx[mx.length - 1 - i][mid];
-                // bottom <- left
-                mx[mx.length - 1 - i][mid] = mx[mid][i];
-                // left <- top
-                mx[mid][i] = tmp;
-            }
         }
     }
 }
