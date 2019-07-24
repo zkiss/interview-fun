b7859532aafee1d98885064355594b7ca749fd5c
Zoltan Kiss <zoltan.kiss.jr@gmail.com>
2019-07-24 23:05:40 +0100

    GenerateParentheses

---
+++ src/main/java/kissz/GenerateParentheses.java
@@ -0,0 +1,27 @@
+package kissz;
+
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+
+public class GenerateParentheses {
+    public List<String> generateParenthesis(int n) {
+        List[] solutions = new List[n + 1];
+        solutions[0] = Arrays.asList("");
+        return gen(solutions, n);
+    }
+
+    private List<String> gen(List[] solutions, int n) {
+        if (solutions[n] != null) return solutions[n];
+
+        List<String> sn = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (String inner : gen(solutions, i))
+                for (String remaining : gen(solutions, n - 1 - i))
+                    sn.add("(" + inner + ")" + remaining);
+        }
+        solutions[n] = sn;
+
+        return solutions[n];
+    }
+}
